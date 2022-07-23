2ac8c1ea9137ec813ff57d0888ca137b2cc8c7ff
tgd <zjrzpp0414@163.com>
2022-07-23 20:41:51 +0800

    feat:modify run port

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3001
+  },
   plugins: [react()],
   css: {
     // css模块化: 文件以.module.[css|less|scss]结尾
